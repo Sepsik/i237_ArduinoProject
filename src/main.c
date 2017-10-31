d3266930b556233faadafe8b3e06e6eda3f6c5a4
Anita Sepp <anita@Anitas-MacBook-Pro.local>
2017-10-31 23:44:58 +0200

    Added lab2 code

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
-#define __AVR_ATmega328P__ 1
+#include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BLINK_DELAY_MS 1000
+#define BLINK_DELAY_MS 300
 
-void blink (int port)
+void blink (char port)
 {
     PORTB |= _BV(port);
     _delay_ms(BLINK_DELAY_MS);
@@ -12,17 +12,27 @@ void blink (int port)
     _delay_ms(BLINK_DELAY_MS);
 }
 
+
 void main (void)
 {
-    DDRB |= _BV(DDB3);
+    DDRB |= _BV(DDB2);
     DDRB |= _BV(DDB4);
     DDRB |= _BV(DDB5);
+    DDRB |= _BV(DDB7);
 
     while (1) {
-        blink(PORTB3);
-        blink(PORTB4);
-        blink(PORTB5);
+        switch (rand() % 3) {
+        case 0:
+            blink(PORTB2);
+            break;
+
+        case 1:
+            blink(PORTB4);
+            break;
+
+        case 2:
+            blink(PORTB5);
+            break;
+        }
     }
 }
-
-
