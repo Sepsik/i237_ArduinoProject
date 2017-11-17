7b920a86a94becc9f2a2ae7cf4a71b47444d449a
Anita Sepp <anita@Anitas-MacBook-Pro.local>
2017-11-17 16:53:01 +0200

    added lab3 code

--- src/main.c
+++ src/main.c
@@ -1,10 +1,35 @@
-#include <stdlib.h>
+#include <stdio.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include "uart.h"
 
 #define BLINK_DELAY_MS 300
 
-void blink (char port)
+
+static inline void init_leds(void)
+{
+    DDRB |= _BV(DDB2);
+    DDRB |= _BV(DDB4);
+    DDRB |= _BV(DDB5);
+    DDRB |= _BV(DDB7);
+}
+
+
+/* Init error console as stderr in UART1 and print user code info */
+static inline void init_errcon(void)
+{
+    simple_uart1_init();
+    stderr = &simple_uart1_out;
+    fprintf(stderr, "Version: %s built on: %s %s\n",
+            FW_VERSION, __DATE__, __TIME__);
+    fprintf(stderr, "avr-libc version: %s avr-gcc version: %s\n",
+            __AVR_LIBC_VERSION_STRING__, __VERSION__);
+}
+
+
+static inline void blink_led(char port)
 {
     PORTB |= _BV(port);
     _delay_ms(BLINK_DELAY_MS);
@@ -13,26 +38,50 @@ void blink (char port)
 }
 
 
+static inline void blink_leds(void)
+{
+    switch (rand() % 3) {
+    case 0:
+        blink_led(PORTB2);
+        break;
+
+    case 1:
+        blink_led(PORTB4);
+        break;
+
+    case 2:
+        blink_led(PORTB5);
+        break;
+    }
+}
+
+
 void main (void)
 {
-    DDRB |= _BV(DDB2);
-    DDRB |= _BV(DDB4);
-    DDRB |= _BV(DDB5);
-    DDRB |= _BV(DDB7);
+    init_leds();
+    init_errcon();
+    /* Test assert - REMOVE IN FUTURE LABS */
+    char *array;
+    uint32_t i = 1;
+    extern int __heap_start, *__brkval;
+    int v;
+    array = malloc( i * sizeof(char));
+    assert(array);
+    /* End test assert */
 
     while (1) {
-        switch (rand() % 3) {
-        case 0:
-            blink(PORTB2);
-            break;
-
-        case 1:
-            blink(PORTB4);
-            break;
-
-        case 2:
-            blink(PORTB5);
-            break;
-        }
+        blink_leds();
+        //_delay_ms(BLINK_DELAY_MS);
+        /* Test assert - REMOVE IN FUTURE LABS */
+        /*
+         * Increase memory allocated for array by 100 chars
+         * until we have eaten it all and print space between stack and heap.
+         * That is how assert works in run-time.
+         */
+        array = realloc( array, (i++ * 100) * sizeof(char));
+        fprintf(stderr, "%d\n",
+                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
+        assert(array);
+        /* End test assert */
     }
 }
